6b24cdf0166726c45c8baa29b2873d65eaa0ea49
Kathleen Hamrick <kchamrick@gmail.com>
2025-01-25 16:23:02 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-// Write your Color component here
+import {useState} from 'react'; //imports useState hook from React
 
-const App = () => {
-  return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
+const Color = ({color, setSelectedColor, selectedColor}) => { //color component with props for color, setSelectedColor function, and selectedColor state
+
+  const classes = `${color} ${selectedColor === color? 'selected' : ''}`; //creates className string and adds 'selected' class if the color is selected
+
+  return (<div 
+  className={classes}
+  onClick={() => setSelectedColor(color)} //click handler to update selected color
+  ></div>
   );
 };
 
-export default App;
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState(""); //app component with selectedColor state to empty string
+
+  return ( //returns container with navbar showing color selection
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+      <div id="colors-list">
+        <Color color="yellow" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="orange" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="violet" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/> 
+        <Color color="blue" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/> 
+        <Color color="green" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/> 
+        <Color color="white" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/> 
+        </div> 
+    </div>
+  ); //renders three color components with props for color, setter function, and current selection
+};
+
+export default App; //exports app component
